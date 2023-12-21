a4428b9aef5bc0cf3400d4ae46cd90b81c9c22db
anggifit <abonaldy@gmail.com>
2023-12-21 17:49:00 +0100

    adding custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'primary': '#BD57FC',
+      'secondary': '#F9F3FD',
+      'text-primary': '#000000',
+      'text-secondary': '#FFFFFF',
+      'button-section': '#822178',
+    },
   },
   plugins: [],
 }
